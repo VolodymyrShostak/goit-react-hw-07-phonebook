d46d06ee32cf87d5ab40a31bbc516b615f6337d7
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-10 12:41:01 +0200

    change index.js

---
+++ src/components/ContactsList/operation.js
@@ -0,0 +1,5 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const postsRequest = createAsyncThunk('contacts/fetchAll', async (data, thunkAPI ) => {
+    
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import store, { persistor } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
 import { Provider } from 'react-redux';
 import { App } from './components/App.jsx';
 import './index.css';
@@ -9,9 +8,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
     ,
   </React.StrictMode>
