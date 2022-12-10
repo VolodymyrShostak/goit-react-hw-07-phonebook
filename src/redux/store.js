36a03ec4c5ddb7909a28784a7d6c07512f09905e
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-12-10 13:39:17 +0200

    create reduser (start)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,23 @@
 import { contactsReducer } from './personDataSlice';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filterSlise';
 
-const phonebookPersistConfig = {
-  key: 'phonebook',
-  storage,
-  whitelist: ['contacts'],
-};
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-const persistedReduser = persistReducer(phonebookPersistConfig, rootReducer);
-const store = configureStore({
-  reducer: persistedReduser,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+export const store = configureStore({
+  contacts: {
+    items: [],
+    isLoading: false,
+    error: null
+  },
+  filter: ""
+}
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+//   filter: filterReducer,
+// });
 
-export const persistor = persistStore(store);
-export default store;
+
+  
+);
+
+
+
